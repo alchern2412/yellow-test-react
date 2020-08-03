0f3084fe238c078cb270b9f7882f802f54fd8bed
alchern2412 <mynameislyosha2412@yandex.com>
2020-08-03 23:59:49 +0300

    add Login window

---
+++ src/containers/Login/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Login.scss'
+
+const Login = props => {
+    return (
+        <div className="login">
+            <div className="login-wrapper">
+                <div className="login__icon"></div>
+                <div className="login__btn"></div>
+            </div>
+        </div>
+    )
+}
+
+Login.propTypes = {
+
+}
+
+export default Login
