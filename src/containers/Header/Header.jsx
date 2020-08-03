ec3179ab1c5683c2b44c6a5a9a42ed0e91735747
alchern2412 <mynameislyosha2412@yandex.com>
2020-08-03 23:34:27 +0300

    add Header, font SFUIText, logo

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import './Header.scss'
+
+const Header = ({
+    auth: {
+        isAuthenticated,
+        loading
+    }
+}) => {
+    const authLinks = [
+        { name: 'Jogs', path: '/jogs' },
+        { name: 'Info', path: '/info' },
+        { name: 'Contact Us', path: '/contact-us' },
+    ]
+    return (
+        <header className="header">
+            {/* <div className="wrapper"> */}
+                <div className="header__wrapper">
+                    <div className="header__logo">
+                        <Link to="/">
+                            <div  className="header__logo-link" />
+                        </Link>
+                    </div>
+                    <nav className="header__nav">
+                        <ul className="header__list">
+                            {
+                                !loading && isAuthenticated && authLinks.map(link => (
+                                    <li key={link.name} className="header__item">
+                                        <Link to={ link.path } className="header__link">{ link.name }</Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                </div>
+            {/* </div> */}
+        </header>
+    )
+}
+
+Header.propTypes = {
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(Header)
