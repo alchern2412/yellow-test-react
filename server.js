db0c6496a02a93a2b1c5dc5ba486cfea7dad1523
alchern2412 <mynameislyosha2412@yandex.com>
2020-08-06 00:06:37 +0300

    add heroku deploying preparation

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+// Init Middleware
+app.use(express.json({ extended: false }))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder 
+  app.use(express.static('build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
+
+
+
+
+
