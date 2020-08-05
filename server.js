5faf9f2f2e9177385f533e3193c6f807fa5e4637
alchern2412 <mynameislyosha2412@yandex.com>
2020-08-06 00:33:00 +0300

    fix add static to server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const path = require('path')
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-
 app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 // Serve static assets in production
 app.get('/*', (req, res) => {
